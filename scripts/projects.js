5daf57f5a53d4e5ebd5d17137a11b1c409d33f6c
Hexuar <hexuar@gmail.com>
2025-05-04 23:10:11 +0200

    Renamed 'pages' to 'projects'

---
+++ scripts/projects.js
@@ -0,0 +1,31 @@
+const projectList = document.getElementById("projectList");
+const template = document.getElementById("projectEntry");
+
+function AddToProjectList(project) {
+    const clone = template.content.cloneNode(true);
+
+    let div = clone.querySelector("li");
+    div.href = "./projects/" + project.id + "/index.html";
+    div.onclick = function () {
+        location.href = this.href;
+    };
+
+    let elements = clone.querySelectorAll("p");
+    elements[0].innerHTML = project.title;
+    elements[1].innerHTML = project.description;
+    elements[2].innerHTML = project.created;
+
+    projectList.appendChild(clone);
+}
+
+// Load all pages
+fetch("./projects/projects.json")
+    .then((result) => (data = result.json()))
+    .then((data) => {
+        data.sort(function (a, b) {
+            return new Date(b.created) - new Date(a.created);
+        });
+        data.forEach((project) => {
+            AddToProjectList(project);
+        });
+    });
