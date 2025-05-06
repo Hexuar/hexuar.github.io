457306be6fd37dfa6a4039b27ca2386d44c3ced1
Hexuar <hexuar@gmail.com>
2025-05-06 17:36:47 +0200

    Info boxes

--- scripts/loadproject.js
+++ scripts/loadproject.js
@@ -1,7 +1,26 @@
 const paramString = window.location.search;
 const searchParams = new URLSearchParams(paramString);
 
-const iframe = document.createElement("iframe");
-iframe.src = "./projects/" + searchParams.get("id") + "/index.html";
-iframe.classList.add("project");
-document.body.appendChild(iframe);
+const id = searchParams.get("id")
+if(id != undefined) {
+    const iframe = document.createElement("iframe");
+    iframe.src = "./projects/" + id + "/index.html";
+    iframe.classList.add("project");
+    document.body.appendChild(iframe);
+}
+
+const infoBox = document.getElementById("infoBox");
+fetch("./projects/projects.json")
+    .then((result) => (data = result.json()))
+    .then((data) => {
+        let project = data.find(obj => { return obj.id == id });
+
+        if(project.info == undefined) {
+            infoBox.hidden = true;
+            return;
+        }
+
+        let elements = infoBox.querySelectorAll("p");
+        elements[0].innerHTML = project.title;
+        elements[1].innerHTML = project.info || "";
+    });
