7f4305cd8b24d424cdf298efcbac65f77e143ae5
Hexuar <hexuar@gmail.com>
2025-08-16 18:16:04 +0200

    Scantime

--- projects/gps_accuracy/script.js
+++ projects/gps_accuracy/script.js
@@ -1,9 +1,12 @@
+const statusP = document.getElementById("status");
 const p = document.getElementById("container");
-
 const data = [];
+const scanTime = prompt("Scantime: ") * 1000;
+const startTime = new Date().getTime();
+let ID;
 
 if("geolocation" in navigator) {
-    navigator.geolocation.watchPosition((position) => {
+    ID = navigator.geolocation.watchPosition((position) => {
         data.push(Math.round(position.coords.accuracy));
         p.textContent = "data = {" + data + "}";
     });
@@ -11,3 +14,18 @@ if("geolocation" in navigator) {
 else {
     p.textContent = "Geolocation not avaliable.";
 }
+
+function Update() {
+    let time = new Date().getTime();
+
+    if(time - startTime > scanTime) {
+        navigator.geolocation.clearWatch(ID);
+        statusP.textContent = "Complete!";
+    }
+    else {
+        statusP.textContent = "Scanning... " + Math.round((time - startTime) / 1000);
+    }
+
+    requestAnimationFrame(Update);
+}
+Update();
