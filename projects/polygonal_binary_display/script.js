e22d8d7e1881b1e439fb81a3175b724acef2804c
Hexuar <hexuar@gmail.com>
2026-05-07 17:58:07 +0200

    More k values

--- projects/polygonal_binary_display/script.js
+++ projects/polygonal_binary_display/script.js
@@ -1,26 +1,54 @@
 // UI
 const nodeSlider = document.querySelector("#nodeSlider");
-const numberSlider = document.querySelector("#numberSlider");
+const numberSliders = document.querySelector("#numberSliders").querySelectorAll("number-slider");
 
-numberSlider.max = Math.pow(2, nodeSlider.value) - 1;
-nodeSlider.addEventListener("input", () => {
-  numberSlider.max = Math.pow(2, nodeSlider.value) - 1;
-});
 
+function setNumberSliderMax() {
+  numberSliders.forEach(slider => {
+    slider.max = Math.pow(2, nodeSlider.value) - 1;
+  });
+}
+
+function setKValues() {
+  kValues = [1];
+  for (i = 2; i < nodeSlider.value / 2; i++) {
+    if (nodeSlider.value % i != 0) kValues.push(i);
+  }
+}
+
+function setNumberSliderDisplay() {
+  for (i = 1; i < numberSliders.length; i++) {
+    if (i < kValues.length) numberSliders[i].style.display = "block";
+    else numberSliders[i].style.display = "none";
+  }
+}
 
 function RandomizeValues() {
   let hue = 360 * Math.random();
-  COLORS = ["hsl("+ hue + " 20% 25%)", "hsl(" + (hue + 180) + " 20% 50%)"]
+  COLORS = ["hsl(" + hue + " 20% 25%)", "hsl(" + (hue + 180) + " 20% 50%)"];
 
   nodeSlider.value = Math.randInt(nodeSlider.max - 2) + 3;
-  numberSlider.max = Math.pow(2, nodeSlider.value) - 1;
-  numberSlider.value = Math.randInt(numberSlider.max) + 1;
-
   nodeSlider.style.setProperty("--background", COLORS[0]);
-  numberSlider.style.setProperty("--background", COLORS[0]);
   nodeSlider.style.setProperty("--foreground", COLORS[1]);
-  numberSlider.style.setProperty("--foreground", COLORS[1]);
+
+  numberSliders.forEach(slider => {
+    slider.max = Math.pow(2, nodeSlider.value) - 1;
+    slider.value = Math.randInt(slider.max) + 1;
+    slider.style.setProperty("--background", COLORS[0]);
+    slider.style.setProperty("--foreground", COLORS[1]);
+  });
+
+  setKValues();
+  setNumberSliderDisplay();
 }
+
+
+// Init
+nodeSlider.addEventListener("input", () => {
+  setNumberSliderMax();
+  setKValues();
+  setNumberSliderDisplay();
+});
 document.addEventListener("keydown", (event) => {
   if (event.key === " ") RandomizeValues();
 });
@@ -33,20 +61,25 @@ function Update() {
 
   N = nodeSlider.value;
   nodeRadius = canvas.height / 40;
-  numberString = parseInt(numberSlider.value).toString(2);
-  for (i = 0; numberString.length < N; i++) {
-    numberString = "0" + numberString;
-  }
 
-  for (i = 0; i < N; i++) {
-    if (numberString[i] == "1") {
-      StrokeLine(Vec2.add(canvas.center, Vec2(canvas.height / 3, 2 * Math.PI / N * i - Math.PI / 2, true)), Vec2.add(canvas.center, Vec2(canvas.height / 3, 2 * Math.PI / N * (i + 1) - Math.PI / 2, true)), COLORS[1], 0.33 * nodeRadius);
+  for (i = 0; i < kValues.length; i++) {
+    k = kValues[i];
+
+    numberString = parseInt(numberSliders[i].value).toString(2);
+    for (j = 0; numberString.length < N; j++) {
+      numberString = "0" + numberString;
+    }
+
+    for (j = 0; j < N; j++) {
+      if (numberString[j] == "1") {
+        StrokeLine(Vec2.add(canvas.center, Vec2(canvas.height / 3, 2 * Math.PI / N * k * j - Math.PI / 2, true)), Vec2.add(canvas.center, Vec2(canvas.height / 3, 2 * Math.PI / N * k * (j + 1) - Math.PI / 2, true)), COLORS[1], 0.33 * nodeRadius);
+      }
     }
   }
 
+  // Vertices
   for (i = 0; i < N; i++) {
     FillCircle(Vec2.add(canvas.center, Vec2(canvas.height / 3, 2 * Math.PI / N * i - Math.PI / 2, true)), nodeRadius, COLORS[1]);
   }
-
   FillCircle(Vec2.add(canvas.center, Vec2(0, -canvas.height / 3)), 0.67 * nodeRadius, COLORS[0])
 }
