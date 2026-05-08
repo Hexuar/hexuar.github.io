29005cda11fd921a912cd53e3e7accb3feae55e9
Hexuar <hexuar@gmail.com>
2026-05-08 17:54:46 +0200

    Animation

---
+++ unpublished/animation/script.js
@@ -0,0 +1,41 @@
+const color = "hsl(389.7748540242311 20% 50%)";
+let t = 0;
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    t = 0.0001;
+  }
+});
+
+
+function smoothstep(t) {
+  return t * t * (3 - 2 * t);
+}
+
+
+function lerp(a, b, t) {
+  return Vec2.add(Vec2.mul(a, 1 - t), Vec2.mul(b, t));
+}
+
+function smerp(a, b, t) {
+  return lerp(a, b, smoothstep(t));
+}
+
+function qerp(a, b, t) {
+  return lerp(a, b, t * t);
+}
+
+
+function Update() {
+  Fill("hsl(209.77485402423108 20% 25%)");
+
+  if (t < 1 && t > 0) {
+    t += 0.01;
+  }
+
+  let A = Vec2(0.1 * canvas.width, canvas.height / 2);
+  let B = Vec2(0.9 * canvas.width, canvas.height / 2);
+
+  FillCircle(qerp(A, B, t), 25, color);
+  StrokeLine(A, B, color, 10, "round");
+}
