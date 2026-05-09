744aeb95e1efce14d50c5f97558db4be3c7fa59e
Hexuar <hexuar@gmail.com>
2026-05-09 20:51:21 +0200

    Cleaned up code

--- projects/polygonal_binary_display/script.js
+++ projects/polygonal_binary_display/script.js
@@ -1,25 +1,19 @@
-CreateCanvasLayer();
-
-// UI
 const nodeSlider = document.querySelector("#nodeSlider");
 const numberSliders = document.querySelector("#numberSliders").querySelectorAll("number-slider");
-const uiShadowAmplitude = 5;
-let lightDir = Vec2(0.5, 0.5);
+
+const lightDir = Vec2(0.5, 0.5);
 let shadowsEnabled = true;
 let nodeRadius = 0;
+let nodePositions = [];
+let kValues = [];
 
 
-function SetNumberSliderMax() {
-    numberSliders.forEach(slider => {
-        slider.max = Math.pow(2, nodeSlider.value) - 1;
-    });
-}
-
+// Calculates current allowed k values
 function SetKValues() {
     kValues = [1];
     for (k = 2; k < nodeSlider.value / 2; k++) {
         if (nodeSlider.value % k != 0) {
-            passed = true;
+            let passed = true;
             for (x = 1; x < k; x++) {
                 if (Number.isInteger(nodeSlider.value / k * x) && nodeSlider.value / k * x < nodeSlider.value) {
                     passed = false;
@@ -31,31 +25,38 @@ function SetKValues() {
     }
 }
 
-function SetNumberSliderDisplay() {
+
+// Sets slider visibility depending on enabled k values
+function SetSliderVisibility() {
     for (i = 1; i < numberSliders.length; i++) {
         if (i < kValues.length) numberSliders[i].style.display = "block";
         else numberSliders[i].style.display = "none";
     }
 }
 
+
+// Randomises colours and slider values
 function RandomizeValues() {
+    // Colors
     let hue = 360 * Math.random();
     COLORS = ["hsl(" + hue + " 20% 25%)", "hsl(" + (hue + 180) + " 20% 50%)", "hsl(" + hue + " 20% 15%)"];
-
-    nodeSlider.value = Math.randInt(nodeSlider.max - 2) + 3;
     document.body.style.setProperty("--background", COLORS[0]);
     document.body.style.setProperty("--foreground", COLORS[1]);
     document.body.style.setProperty("--shadow", COLORS[2]);
 
+    // Sliders
+    nodeSlider.value = Math.randInt(nodeSlider.max - 2) + 3;
     numberSliders.forEach(slider => {
         slider.max = Math.pow(2, nodeSlider.value) - 1;
         slider.value = Math.randInt(slider.max) + 1;
     });
 
     SetKValues();
-    SetNumberSliderDisplay();
+    SetSliderVisibility();
 }
 
+
+// Drawing functions with shadows if shadowsEnabled == true
 function DrawNode(pos, hollow = false) {
     if (hollow) {
         StrokeCircle(pos, 0.835 * nodeRadius, COLORS[1], 0.33 * nodeRadius, "round", 1);
@@ -66,23 +67,26 @@ function DrawNode(pos, hollow = false) {
         if (shadowsEnabled) FillCircle(Vec2.add(pos, Vec2.mul(lightDir, nodeRadius)), nodeRadius, COLORS[2]);
     }
 }
-
 function DrawLine(a, b) {
     let d = Vec2.normalize(Vec2.sub(b, a)).mul(nodeRadius - 1)
     StrokeLine(Vec2.add(a, d), Vec2.sub(b, d), COLORS[1], 0.33 * nodeRadius, "round", 1);
     if(shadowsEnabled) StrokeLine(Vec2.add(a, Vec2.mul(lightDir, nodeRadius)).add(d), Vec2.add(b, Vec2.mul(lightDir, nodeRadius)).sub(d), COLORS[2], 0.33 * nodeRadius);
 }
 
+
+// Linear interpolation of vectors
 function lerp(a, b, t) {
   return Vec2.add(Vec2.mul(a, 1 - t), Vec2.mul(b, t));
 }
 
 
-// Init
+// Event Listeners
 nodeSlider.addEventListener("input", () => {
-    SetNumberSliderMax();
+    numberSliders.forEach(slider => {
+        slider.max = Math.pow(2, nodeSlider.value) - 1;
+    });
     SetKValues();
-    SetNumberSliderDisplay();
+    SetSliderVisibility();
 });
 document.addEventListener("keydown", (event) => {
     if (event.key === " ") RandomizeValues();
@@ -90,48 +94,49 @@ document.addEventListener("keydown", (event) => {
 });
 document.addEventListener("touchstart", (event) => {
     if (event.touches.length == 2) RandomizeValues();
+    else if (event.touches.length == 3) shadowsEnabled = !shadowsEnabled;
 });
+
+
+// Init
+CreateCanvasLayer();
 RandomizeValues();
 
 
 // Update
-let nodePositions = [];
 function Update(deltaTime) {
-    Clear();
+    // Clear canvas
     Clear(1);
     Fill(COLORS[0]);
 
-
-    let N = nodeSlider.value;
+    // Calculate size
     let size = Math.min(canvas.width, canvas.height);
     nodeRadius = size / 40;
-    document.body.style.setProperty("--lightDir", uiShadowAmplitude * lightDir.x + "px " + uiShadowAmplitude * lightDir.y + "px")
 
-    for (i = 0; i < N; i++) {
-        let target = Vec2.add(canvas.center, Vec2(size / 3, - 2 * Math.PI / N * i - Math.PI / 2, true));
+    // Set node positions
+    for (i = 0; i < nodeSlider.value; i++) {
+        let target = Vec2.add(canvas.center, Vec2(size / 3, - 2 * Math.PI / nodeSlider.value * i - Math.PI / 2, true));
         nodePositions[i] = nodePositions[i] == undefined ? target : lerp(nodePositions[i], target, 10 * deltaTime);
     }
-
 
     // Lines
     for (i = 0; i < kValues.length; i++) {
         k = kValues[i];
 
         numberString = parseInt(numberSliders[i].value).toString(2);
-        for (j = 0; numberString.length < N; j++) {
+        for (j = 0; numberString.length < nodeSlider.value; j++) {
             numberString = "0" + numberString;
         }
 
-        for (j = 0; j < N; j++) {
+        for (j = 0; j < nodeSlider.value; j++) {
             if (numberString[j] == "1") {
-                DrawLine(nodePositions[(k * j) % N], nodePositions[(k * (j + 1)) % N]);
+                DrawLine(nodePositions[(k * j) % nodeSlider.value], nodePositions[(k * (j + 1)) % nodeSlider.value]);
             }
         }
     }
 
-
     // Nodes
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < nodeSlider.value; i++) {
         DrawNode(nodePositions[i], i == 0);
     }
 }
