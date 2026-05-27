05a4534587fd61e4a620651691170ebffd34efac
Hexuar <hexuar@gmail.com>
2026-05-28 01:24:18 +0200

    Keyboard input

--- unpublished/procedural_animation/script.js
+++ unpublished/procedural_animation/script.js
@@ -17,30 +17,33 @@ class Segment {
 }
 
 
-let n = 20;
+let speed = 5;
+let turnspeed = 0.03;
+let n = 30;
 let segments = [];
 for (i = 0; i < n; i++) {
-    segments.push(new Segment(Vec2(200, 200 + 1.5 * 50 * i), n - i, 50));
+    segments.push(new Segment(Vec2(200, 200 + 1.5 * 50 * i), n - i + 5, 50));
 }
 segments[0].direction = Vec2(1, 0);
 
 
-function Spline(points = [], strength = 8, color) {
-    let region = new Path2D();
-    region.moveTo(points[1].x, points[1].y);
-    for (i = 1; i < points.length - 2; i++) {
-        let a1 = Vec2.sub(points[i], points[i - 1]);
-        let b1 = Vec2.sub(points[i], points[i + 1]);
-        let c1 = Vec2.add(points[i], Vec2.sub(a1, b1).normalize().mul((a1.length + b1.length) / strength));
-        let a2 = Vec2.sub(points[i + 1], points[i]);
-        let b2 = Vec2.sub(points[i + 1], points[i + 2]);
-        let c2 = Vec2.add(points[i + 1], Vec2.sub(b2, a2).normalize().mul((a1.length + b1.length) / strength));
-        region.bezierCurveTo(c1.x, c1.y, c2.x, c2.y, points[i + 1].x, points[i + 1].y);
-    }
-    region.closePath();
-    ctx.strokeStyle = "black";
+function Spline(points = [], color) {
     ctx.fillStyle = color;
-    ctx.fill(region);
+    ctx.beginPath();
+    ctx.moveTo(points[0].x, points[0].y);
+    for (i = 0; i < points.length; i++) {
+        p0 = (i == 0) ? points[points.length - 1] : points[i - 1];
+        p = points[i];
+        p1 = (i == points.length - 1) ? points[0] : points[i + 1];
+        p2 = (i >= points.length - 2) ? points[i - points.length + 2] : points[i + 2];
+        let a = Vec2.sub(p1, p0).div(6);
+        let b = Vec2.sub(p2, p).div(6);
+        let A = Vec2.add(p, a);
+        let B = Vec2.sub(p1, b);
+        ctx.bezierCurveTo(A.x, A.y, B.x, B.y, p1.x, p1.y)
+    }
+    ctx.closePath();
+    ctx.fill();
 }
 
 
@@ -48,27 +51,28 @@ function Update(deltaTime) {
     Fill("hsl(209.77485402423108 20% 25%)");
 
 
-    segments[0].direction = Vec2.sub(mouse.pos, segments[0].position).normalize();
-    let D = Vec2.sub(mouse.pos, segments[0].position);
-    if(D.r > 2) segments[0].position = Vec2.add(segments[0].position, D.div(2));
+    if (keyboard.held("w")) segments[0].direction.add(Vec2(0, -turnspeed));
+    if (keyboard.held("a")) segments[0].direction.add(Vec2(-turnspeed, 0));
+    if (keyboard.held("s")) segments[0].direction.add(Vec2(0, turnspeed));
+    if (keyboard.held("d")) segments[0].direction.add(Vec2(turnspeed, 0));
+    segments[0].direction.normalize();
+    segments[0].position.add(Vec2.mul(segments[0].direction, speed));
 
-    for (i = 0; i < n; i++) {
+    for (i = 1; i < n; i++) {
         let s = segments[i];
-        if (i != 0) {
-            let p = segments[i - 1];
-            s.direction = Vec2.sub(p.position, s.position).normalize();
-            s.position = Vec2.sub(p.position, Vec2.mul(s.direction, p.distance));
-        }
+        let p = segments[i - 1];
+        s.direction = Vec2.sub(p.position, s.position).normalize();
+        s.position = Vec2.sub(p.position, Vec2.mul(s.direction, p.distance));
     }
 
-    let nodes = [segments[1].right, segments[0].right];
-    for (i = 0; i < n; i++) {
+    let nodes = [Vec2.add(segments[0].position, Vec2.mul(segments[0].direction, segments[0].radius))];
+    for (i = 0; i < segments.length; i++) {
         nodes.push(segments[i].left);
     }
-    for (i = n - 1; i >= 0; i--) {
+    nodes.push(Vec2.sub(segments[segments.length - 1].position, Vec2.mul(segments[segments.length - 1].direction, 3 * segments[segments.length - 1].radius)));
+    for (i = segments.length - 1; i >= 0; i--) {
         nodes.push(segments[i].right);
     }
-    nodes.push(segments[0].left)
 
-    Spline(nodes, 8, color);
+    Spline(nodes, color);
 }
