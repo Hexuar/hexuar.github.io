f5850719d8170c821d3fe011f36fbfc2266b0cc9
Hexuar <hexuar@gmail.com>
2026-06-14 20:24:30 +0200

    Chunks

--- unpublished/particle_life/particle.js
+++ unpublished/particle_life/particle.js
@@ -4,15 +4,62 @@ class Particle {
         this.position = position;
         this.velocity = Vec2();
         this.type = type;
+        this.chunkPosition = null;
 
         particles.push(this);
+        Chunks.Add(this, this.GetChunkCoordinates());
+    }
+    Update() {
+        this.position.add(this.velocity);
+        this.velocity.mul(1 - FRICTION);
+
+        if (!this.IsWithinChunkBoundaries(this.chunkPosition)) {
+            Chunks.Remove(this, this.chunkPosition);
+            Chunks.Add(this, this.GetChunkCoordinates(this));
+        }
+    }
+    Draw(cameraPos, zoom) {
+        FillCircle(Vec2.sub(this.position, cameraPos).mul(zoom).add(canvas.center), 3 * zoom, colors[this.type].toString());
     }
     GetInteraction(particle) {
         let f = interactionMatrix[this.type][particle.type];
         let d = Vec2.sub(particle.position, this.position);
         return d.normalize().mul(f / Vec2.sub(this.position, particle.position).lengthSquared);
     }
-    Draw(cameraPos, zoom) {
-        FillCircle(Vec2.add(this.position, cameraPos).mul(zoom).add(canvas.center), 3 * zoom, colors[this.type].toString());
+    IsWithinChunkBoundaries(chunkPosition) {
+        return (Math.floor(this.position.x / CHUNK_SIZE) == chunkPosition.x && Math.floor(this.position.y / CHUNK_SIZE) == chunkPosition.y);
+    }
+    GetChunkCoordinates() {
+        return Vec2(Math.floor(this.position.x / CHUNK_SIZE), Math.floor(this.position.y / CHUNK_SIZE));
+    }
+    GetNearbyParticles() {
+        let particles = [];
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                let chunkPosition = Vec2.add(this.chunkPosition, Vec2(i, j))
+                particles = particles.concat(Chunks.Get(chunkPosition));
+            }
+        }
+        return particles;
     }
 }
+
+const Chunks = {
+    Get : function(chunkPosition) {
+        let data = Chunks[chunkPosition.toString()];
+        if (data == undefined) data = [];
+        return data;
+    },
+    Add : function(particle, chunkPosition) {
+        let data = Chunks.Get(chunkPosition);
+        data.push(particle);
+        Chunks[chunkPosition.toString()] = data;
+        particle.chunkPosition = chunkPosition;
+    },
+    Remove : function(particle, chunkPosition) {
+        let data = Chunks.Get(chunkPosition);
+        data.pop(data.indexOf(particle));
+        Chunks[chunkPosition.toString()] = data;
+        particle.chunkPosition = null;
+    }
+};
--- unpublished/particle_life/script.js
+++ unpublished/particle_life/script.js
@@ -1,12 +1,13 @@
 const SIMULATION_SPEED = 2000;
 const TYPE_COUNT = 5;
-const PARTICLE_COUNT = 1500;
+const PARTICLE_COUNT = 2000;
 const FRICTION = 0.1;
 const CAMERA_SPEED = 1000;
+const CHUNK_SIZE = 100;
 const MIN_RADIUS = Math.sq(20);
-const MAX_RADIUS = Math.sq(100);
+const MAX_RADIUS = Math.sq(CHUNK_SIZE);
 const OPACITY = 0.5;
-let cameraPosition = Vec2().sub(canvas.center);
+let cameraPosition = Vec2().add(canvas.center);
 let zoom = 0.75;
 let colors = [];
 let interactionMatrix = [];
@@ -35,33 +36,29 @@ Init();
 function Update(deltaTime) {
     Fill("black");
 
-    if (keyboard.held("w")) cameraPosition.y += CAMERA_SPEED * deltaTime;
-    if (keyboard.held("a")) cameraPosition.x += CAMERA_SPEED * deltaTime;
-    if (keyboard.held("s")) cameraPosition.y -= CAMERA_SPEED * deltaTime;
-    if (keyboard.held("d")) cameraPosition.x -= CAMERA_SPEED * deltaTime;
+    if (keyboard.held("w")) cameraPosition.y -= CAMERA_SPEED * deltaTime;
+    if (keyboard.held("a")) cameraPosition.x -= CAMERA_SPEED * deltaTime;
+    if (keyboard.held("s")) cameraPosition.y += CAMERA_SPEED * deltaTime;
+    if (keyboard.held("d")) cameraPosition.x += CAMERA_SPEED * deltaTime;
 
     if (keyboard.held("q")) zoom += zoom * deltaTime;
     if (keyboard.held("e")) zoom -= zoom * deltaTime;
 
     particles.forEach(p1 => {
-        particles.forEach(p2 => {
+        p1.Update();
+        p1.Draw(cameraPosition, zoom);
+
+        p1.GetNearbyParticles().forEach(p2 => {
             if (p1 === p2) return;
-            if (Math.abs(p1.position.x - p2.position.x) > MAX_RADIUS) return;
-            if (Math.abs(p1.position.y - p2.position.y) > MAX_RADIUS) return;
             let d = Vec2.sub(p1.position, p2.position).lengthSquared;
             if (d < MIN_RADIUS || d > MAX_RADIUS) return;
             p1.velocity.add(p1.GetInteraction(p2).mul(SIMULATION_SPEED * deltaTime));
         });
-
-        p1.position.add(p1.velocity);
-        p1.velocity.mul(1 - FRICTION);
-
-        p1.Draw(cameraPosition, zoom);
     });
 
     t += deltaTime;
-    if (t > 1) {
-        t -= 1;
+    if (t > 0.5) {
+        t -= 0.5;
         console.log("FPS:", Math.round(1 / deltaTime));
     }
 }
